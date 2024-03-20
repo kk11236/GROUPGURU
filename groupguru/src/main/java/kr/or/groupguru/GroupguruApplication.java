74e1bf0b5a3cd2335e1c057fe24428332bdb31bc
kk112 <kk112@172.30.1.50>
2024-03-20 19:04:55 +0900

    0320_KTY GROUPGURU proj first commit

---
+++ groupguru/src/main/java/kr/or/groupguru/GroupguruApplication.java
@@ -0,0 +1,13 @@
+package kr.or.groupguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupguruApplication.class, args);
+	}
+
+}
